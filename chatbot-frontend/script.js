f0ce61f64b7285e8732ffb2756934cf7f7989c9c
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2024-07-10 10:32:04 +0530

    commit message

--- chatbot-frontend/script.js
+++ chatbot-frontend/script.js
@@ -1,7 +1,6 @@
 const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
-const chatbox=document.querySelector(".chatbox")
-
+const chatbox = document.querySelector(".chatbox");
 
 let userMessage;
 
@@ -17,12 +16,42 @@ const createChatLi = (message, className) => {
   return chatLi;
 };
 
+const generateResponse = () => {
+  const API_URL = "https://api.openai.com/v1/chat/completions";
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${API_KEY}`,
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: userMessage }],
+    }),
+  };
+
+//Send POST request to API , get response
+
+  fetch(API_URL,requestOptions).then(res=>res.json()).then(data=>{
+    console.log(data);
+  }).catch((error)=>{
+    console.log(error)
+  })
+};
+
 const handleChat = () => {
   userMessage = chatInput.value.trim();
   if (!userMessage) return;
 
   // Apend the user's message to the chatbox
   chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+
+  setTimeout(() => {
+    //Display "Thinking..." message while waiting for the response
+
+    chatbox.appendChild(createChatLi("Thinking......", "incoming"));
+    generateResponse();
+  }, 600);
 };
 
 sendChatBtn.addEventListener("click", handleChat);
